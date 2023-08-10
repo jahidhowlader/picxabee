ad42c25a98873f5936f411638d91c4965e3bef15
Jahid Howlader <121720284+jahidhowlader@users.noreply.github.com>
2023-08-11 02:43:50 +0600

    check home page (#9)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,21 @@ import Modal from '@/components/Modal';
 import Nav from '@/components/Nav';
 
 export default function Home() {
+<<<<<<< HEAD
+  return(
+    
+    <div className='bg-gray-50 h-screen overflow-y-scroll scrollbar-hide'>
+      {/* Header */}
+      <Nav />
+      
+      {/* Feed  */}
+      <Feed />
+      {/* Modal  */}
+      {/* <Modal /> */}
+    </div>
+    
+  )
+=======
 	return (
 		<div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
 			{/* Header */}
@@ -13,4 +28,5 @@ export default function Home() {
 			<Modal />
 		</div>
 	);
+>>>>>>> e124d16e13a15940be76e0d9699a6363b20f21e4
 }
